696802e8d2d606bf321845e540ab8e96ba07380e
jeongcap <39331162+jeongcap@users.noreply.github.com>
2018-05-24 19:57:35 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,32 +2,42 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cheerio = require('cheerio');
 const request = require('request');
+
 const app = express()
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
+
+
 var popup_link;
 var notice_url_s;
 var notice_url;
+
 var res_content ;
 var req_company = 'kt';
 var req_content = 'FAX';
+
 var url1;
 var url2;
 var url;
+
+
 app.get('/', function (req, res) {
   //  res.send('Use the /webhook endpoint.')
   
   res.send(res_content);
 })
+
 function get_inform(req_company, req_content){
   popup_link = "http://www.saramin.co.kr";
   notice_url = notice_url_s = res_content = '';
   url1 = 'http://www.saramin.co.kr/zf_user/search/company?searchword=';
   url2 = '&searchType=auto&go=';
+
   req_company = encodeURI(req_company); 
   url = url1 + req_company + url2;
   console.log('검색 url 완성');
   
+
   request(url,function(error, response, html){
     console.log(req_company, req_content);
     var $ = cheerio.load(html);
@@ -35,6 +45,7 @@ function get_inform(req_company, req_content){
     var temp = popup_link_info.split("'");
     popup_link += temp[1];
     console.log(popup_link+'    팝업링크 완성');
+
     request(popup_link, function (error, response, html) {
       if (!error) {
           var $1 = cheerio.load(html);
@@ -43,10 +54,12 @@ function get_inform(req_company, req_content){
           b = b.split('\n');
           b.shift();
           b.shift();                
+
           for (var i=0;i <b.length;i++){
               arr[i] = b[i].trim();                    
           }            
           arr = arr.filter(n => n);
+
           console.log(arr[0]+arr[1]+'   내용 배열');
           
           //주어진 req_content 값 찾기
@@ -59,27 +72,77 @@ function get_inform(req_company, req_content){
       });
     
   });  
-}app.post('/webhook', function (req, res) {
+}
+
+
+app.post('/webhook', function (req, res) {
   // we expect to receive JSON data from api.ai here.
   // the payload is stored on req.body
   console.log(req.body)
-  
+  var status = 1;
+  var promise;
   // and some validation too
   if (!req.body.result) {
     return res.status(400).send('bad~!~!')
   }
+
   // parameters are stored in req.body.result.parameters
   req_company = req.body.result.parameters['company'];
   req_content = req.body.result.parameters['Contents'][0];
-  get_inform(req_company,req_content);
-  
-  // the most basic response
-  res.status(200).json({
-    source: 'webhook',
-    speech: res_content,
-    displayText: res_content
+
+  /*new Promise(function(resolve, reject) {
+    // 프라미스 걸어보고
+    // request promise npm module
+    get_inform(req_company,req_content);
+    resolve();
+    //resolve('resolved');
   })
+  .then(function(data) {
+    res.status(200).json({
+      source: 'webhook',
+      speech: res_content,
+      displayText: res_content
+    })
+  })
+  .catch(function(err) {
+    res.status(404).json({
+      error: err
+    });
+  })
+*/
+
+ /* function async (status){
+    return new Promise(function(resolve, reject){
+      if(status){
+        resolve(get_inform(req_company,req_content));
+      }
+      else{
+        reject(res.status(400).send('bad~!~!'));
+      }
+    });
+  }*/
+
+  promise = new Promise(function(resolve, reject){
+    if(status){
+      resolve(get_inform(req.req_company,req.req_content));
+    }
+    else{
+      reject('promise error');
+    }
+  });
+  promise.then(function(result){
+    res.status(200).json({
+      source: 'webhook',
+      speech: result,
+      displayText: result
+    })
+  }, function(err){
+    res.status(400).send(err)
+  });
+  // the most basic response
+  
 })
+
 app.listen(app.get('port'), function () {
   console.log('* Webhook service is listening on port:' + app.get('port'))
 })
